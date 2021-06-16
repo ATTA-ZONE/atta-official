c97fba25d41ac4686c9596acc040486487433b54
mouren <1832819123@qq.com>
2021-06-16 11:51:53 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import {vueI18n} from "@intlify/vite-plugin-vue-i18n";
 export default defineConfig({
-  base: '/web/',
+  base: './',
 	// 静态资源路径
 	publicDir: 'public',
   plugins: [vue(),
