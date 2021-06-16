fb09f962e68c73adae0411c31385495490d1e206
mouren <1832819123@qq.com>
2021-06-16 11:00:57 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,11 @@ import {vueI18n} from "@intlify/vite-plugin-vue-i18n";
 export default defineConfig({
   plugins: [vue(),
     vueI18n({
-    // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-    // compositionOnly: false,
-
-    // you need to set i18n resource including paths !
     include: path.resolve(__dirname, './src/locales/**')
   })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
     },
-  },
-  server: {
-    open: true,
-  },
+  }
 })
