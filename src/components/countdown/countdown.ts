1bed11092008aa10ac97871ad2d1e57be84d4e30
wanglan <809382327@qq.com>
2021-08-22 11:46:53 +0800

    价格宽度

--- src/components/countdown/countdown.ts
+++ src/components/countdown/countdown.ts
@@ -12,7 +12,6 @@ export default defineComponent({
     const modelTips = ref('');
     const MerkleDistributionInstance = getAbi("atta_vote_abi");
     const matchInfoList = ref();
-    const matchconcentList = ref();
     const web3 = ref();
     const matchList = async ()=>{//获取赛事信息列表
       const accounts = await window.CHAIN.WALLET.enable();
@@ -105,7 +104,6 @@ export default defineComponent({
           matchInfoList.value[index].temaAAll = 0;
           matchInfoList.value[index].temaBAll = 0;
         }
-        matchconcentList.value = matchInfoList.value;
       })
     }
     // 对数据进行重装
@@ -168,7 +166,6 @@ export default defineComponent({
         info.temaBAll = 0;
       })
       matchInfoList.value = data;
-      matchconcentList.value = data;
     }    
     const collapseIndex = ref();
     // 展开的时候处理计时器
@@ -517,7 +514,7 @@ export default defineComponent({
       voteStepFn,
       closeDialog,
       moneyFormatNum,
-      matchconcentList,
+      matchInfoList,
       collapseChange,
       nowDataTime,
       curRewardPool,
