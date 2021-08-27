c1e27dd3214537aa9fbc0680ce61306ad1bff134
wanglan <809382327@qq.com>
2021-08-27 14:14:36 +0800

    静态页面

--- src/views/Library/index.ts
+++ src/views/Library/index.ts
@@ -1,4 +1,4 @@
-import { defineComponent } from "vue";
+import { defineComponent,ref } from "vue";
 import headerCell from "@/components/header/index.vue";
 import footerCell from "@/components/footer/index.vue";
 import mediaModal from "@/components/modals/imgPreview.vue";
@@ -8,19 +8,54 @@ export default defineComponent({
       return {
         libraryList:[
           {
-            img01:'/library/libraryTitle/library01.png',//左边大图
+            img01:'/library/libraryTitle/library04.png',//左边大图
             title:'01 丁武音畫系列：實驗音樂 x 動態繪畫',//标题
             text01:'丁武，中國第一代搖滾音樂人，唐朝樂隊核心人物，中國重金屬搖滾樂的先行者和開創者。丁武對藝術的熱愛和追求決定了他的創作並不局限於音符，他對天空，夢境，記憶有這超乎常人的想象，以自身的觀察和經歷為藍本，丁武從未停止對於繪畫的創作。此次丁武攜手香港文創平台和粉絲社區八爪娛，以音樂人域藝術家的雙重身份，透過NFT資產的形勢，將實驗電子域繪畫結合進行全新音視藝術創作，帶領觀眾一同走進他抽象而又激烈的內心世界。',//右侧内容第一段
             text02:'',//右侧内容第二段
-            remark:'备注：用户在Binance NFT市场购买的AATTA NFT编号信息需等到币安NFT提取功能开通后前往ATTA网站MYNFT 板块 查询',//右侧备注
-            logo:'',//右侧logo
+            remark:'備註：用戶在Binance NFT市場購買的ATTA NFT，編號資訊需待幣安NFT提取功能開通後，才可在ATTA網站MYNFT模塊査詢。',//右侧备注
+            logo:'/library/yanran.png',//右侧logo
+            plan01:'计划铸造数量：300',
+            plan02:'BSC网络：',
+            plan03:'ETH网络：',
+            imgsname:'dingwu'
           }
-        ]
+        ],
+        imgsList:{
+          dingwu:[
+            {
+              url:'/library/libraryList/glass (1).png',
+              text:'丁武 - International Remix'
+            },
+            {
+              url:'/library/libraryList/glass (1).png',
+              text:'Wu Ding - International '
+            },
+            {
+              url:'/library/libraryList/glass (1).png',
+              text:'丁武 - International Remix'
+            }
+          ]
+        }
       }
   },
   name: "library",
   props: {
     msg: String,
   },
+  setup() {
+    const moreNumber = ref(0);
+    const moreBtn = (index: number)=>{
+      if(moreNumber.value == index){
+        moreNumber.value = 0;
+      }else{
+        moreNumber.value = index;
+      }
+    }
+    return {
+      moreBtn,
+      moreNumber
+    }
+  }
+
 
 });
--- src/views/Library/index.vue
+++ src/views/Library/index.vue
@@ -2,7 +2,7 @@
   <header-cell />
   <div class="library-page">
     <h5 class="library-title">探索ATTA NFT</h5>
-    <div class="library-list" v-for="item in libraryList">
+    <div class="library-list" v-for="(item,index) in libraryList" :key="index">
       <h5 class="list-title">{{item.title}}</h5>
       <div class="library-list-info flex">
         <div class="library-img">
@@ -14,11 +14,26 @@
             <p>{{item.text02}}</p>
           </div>
           <div class="text-bottom">
-            <img v-if="item.logo" src="" alt="">
-            <p class="text-remark"></p>
+            <p class="text-remark mt-15">{{item.plan01}}</p>
+            <p class="text-remark">{{item.plan02}}</p>
+            <p class="text-remark">{{item.plan03}}</p>
+            <img v-if="item.logo" class="mt-15" :src="item.logo" alt="">
           </div>
         </div>
       </div>
+      <div class="library-more">
+        <div class="more-btn flex" @click="moreBtn(index+1)">
+          <img class="library-icon" :class="moreNumber&&(moreNumber==index+1)?'more-icon':''" src="/match/more.png" alt="">
+          <span>{{moreNumber&&(moreNumber==index+1)?'收起':'展开'}}</span>
+        </div>
+        <div class="more-content flex" :class="moreNumber&&(moreNumber==index+1)?'more-content-more':''">
+          <div class="content-info" v-for="info in imgsList[item.imgsname]">
+            <img :src="info.url" alt="">
+            <p class="hanson">{{info.text}}</p>
+          </div>
+        </div>
+      </div>
+      <p class="library-bottom-remark">{{item.remark}}</p>
     </div>  
   </div>
   <footer-cell />
