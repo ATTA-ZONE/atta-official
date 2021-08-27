0f2675a449916affb048336d105de32577eebc9c
wanglan <809382327@qq.com>
2021-08-27 17:25:04 +0800

    样式优化

--- src/views/Library/index.vue
+++ src/views/Library/index.vue
@@ -23,10 +23,10 @@
           <span>{{moreNumber&&(moreNumber==index+1)?  $t('library_copywriting_116'):  $t('library_copywriting_115')}}</span>
         </div>
         <div class="more-content flex" :class="moreNumber&&(moreNumber==index+1)?'more-content-more':''">
-          <div class="content-info" v-for="(info,num) in item.imgsList" :class="[info.url?'':'img-null',item.imgName?'manghe-info':'']" :key="info.url">
+          <div class="content-info"  @click="openModal(item,index,info,num)" v-for="(info,num) in item.imgsList" :class="[info.url?'':'img-null',item.imgName?'manghe-info':'']" :key="info.url">
             <img :src="info.url" alt="">
             <p class="hanson">{{$t(info.text)}}</p>
-            <img v-if="item.detailName" @click="openModal(item,index,info,num)" class="library-pos" :class="item.detailName=='br'?'pos-br':'pos-tr'" src="/library/detail.png" alt="">
+            <!--<img v-if="item.detailName" class="library-pos" :class="item.detailName=='br'?'pos-br':'pos-tr'" src="/library/detail.png" alt="">-->
           </div>
         </div>
       </div>
