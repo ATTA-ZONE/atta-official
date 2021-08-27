3789f06f95a9d44029d33e70904d88b38387b53d
wangjinyi <wangjinyi143>
2021-08-27 14:32:23 +0800

    修改文案替换

--- src/views/Library/index.ts
+++ src/views/Library/index.ts
@@ -9,30 +9,30 @@ export default defineComponent({
         libraryList:[
           {
             img01:'/library/libraryTitle/library04.png',//左边大图
-            title:'01 丁武音畫系列：實驗音樂 x 動態繪畫',//标题
-            text01:'丁武，中國第一代搖滾音樂人，唐朝樂隊核心人物，中國重金屬搖滾樂的先行者和開創者。丁武對藝術的熱愛和追求決定了他的創作並不局限於音符，他對天空，夢境，記憶有這超乎常人的想象，以自身的觀察和經歷為藍本，丁武從未停止對於繪畫的創作。此次丁武攜手香港文創平台和粉絲社區八爪娛，以音樂人域藝術家的雙重身份，透過NFT資產的形勢，將實驗電子域繪畫結合進行全新音視藝術創作，帶領觀眾一同走進他抽象而又激烈的內心世界。',//右侧内容第一段
-            text02:'',//右侧内容第二段
-            remark:'備註：用戶在Binance NFT市場購買的ATTA NFT，編號資訊需待幣安NFT提取功能開通後，才可在ATTA網站MYNFT模塊査詢。',//右侧备注
+            title:'library_copywriting_2',//标题
+            text01:'library_copywriting_8',//右侧内容第一段
+            remark:'library_copywriting_114',//右侧备注
             logo:'/library/yanran.png',//右侧logo
-            plan01:'计划铸造数量：300',
-            plan02:'BSC网络：',
-            plan03:'ETH网络：',
-            imgsname:'dingwu'
+            imgsname:'dingwu',
           }
         ],
         imgsList:{
           dingwu:[
             {
               url:'/library/libraryList/glass (1).png',
-              text:'丁武 - International Remix'
+              text:'library_copywriting_15'
             },
             {
               url:'/library/libraryList/glass (1).png',
-              text:'Wu Ding - International '
+              text:'library_copywriting_15_2'
             },
             {
               url:'/library/libraryList/glass (1).png',
-              text:'丁武 - International Remix'
+              text:'library_copywriting_15_3'
+            },
+            {
+              url:'/library/libraryList/glass (1).png',
+              text:'library_copywriting_15_4'
             }
           ]
         }
--- src/views/Library/index.vue
+++ src/views/Library/index.vue
@@ -1,22 +1,18 @@
 <template>
   <header-cell />
   <div class="library-page">
-    <h5 class="library-title">探索ATTA NFT</h5>
+    <h5 class="library-title">{{$t('library_copywriting_1')}}</h5>
     <div class="library-list" v-for="(item,index) in libraryList" :key="index">
-      <h5 class="list-title">{{item.title}}</h5>
+      <h5 class="list-title">{{$t(item.title)}}</h5>
       <div class="library-list-info flex">
         <div class="library-img">
           <img :src="item.img01" alt="">
         </div>
         <div class="library-text flex">
           <div class="text-top">
-            <p>{{item.text01}}</p>
-            <p>{{item.text02}}</p>
+            <p v-html="$t(item.text01)"></p>
           </div>
           <div class="text-bottom">
-            <p class="text-remark mt-15">{{item.plan01}}</p>
-            <p class="text-remark">{{item.plan02}}</p>
-            <p class="text-remark">{{item.plan03}}</p>
             <img v-if="item.logo" class="mt-15" :src="item.logo" alt="">
           </div>
         </div>
@@ -24,16 +20,16 @@
       <div class="library-more">
         <div class="more-btn flex" @click="moreBtn(index+1)">
           <img class="library-icon" :class="moreNumber&&(moreNumber==index+1)?'more-icon':''" src="/match/more.png" alt="">
-          <span>{{moreNumber&&(moreNumber==index+1)?'收起':'展开'}}</span>
+          <span>{{moreNumber&&(moreNumber==index+1)?  $t('library_copywriting_116'):  $t('library_copywriting_115')}}</span>
         </div>
         <div class="more-content flex" :class="moreNumber&&(moreNumber==index+1)?'more-content-more':''">
-          <div class="content-info" v-for="info in imgsList[item.imgsname]">
+          <div class="content-info" v-for="info in imgsList[item.imgsname]" :key="info.url">
             <img :src="info.url" alt="">
-            <p class="hanson">{{info.text}}</p>
+            <p class="hanson">{{$t(info.text)}}</p>
           </div>
         </div>
       </div>
-      <p class="library-bottom-remark">{{item.remark}}</p>
+      <p class="library-bottom-remark">{{$t(item.remark)}}</p>
     </div>  
   </div>
   <footer-cell />
