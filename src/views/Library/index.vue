e809677d7c32f2013f9dab614fcc75ee38fef717
mouren <1832819123@qq.com>
2021-08-27 10:17:17 +0800

    no message

--- src/views/Library/index.vue
+++ src/views/Library/index.vue
@@ -1,13 +1,12 @@
 <template>
   <div>dd</div>
+  <mediaModal></mediaModal>
 </template>
 <script lang='ts'>
 import { defineComponent } from "vue";
+import mediaModal from "@/components/modals/imgPreview.vue";
 export default defineComponent({
-  name: "home",
-  props: {
-    msg: String,
-  },
+  components: { mediaModal }
 });
 </script>
 <style scoped lang='scss'>
