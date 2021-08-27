cc4561f020c303c5866ed52103b846afa78b7748
mouren <1832819123@qq.com>
2021-08-27 10:01:44 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -53,6 +53,11 @@ const routes = [
     name: "esports",
     component: esports,
   },
+  {
+    path: "/library",
+    name: "library",
+    component: () => import("../views/Library/index.vue")
+  }
 ];
  
 const router = createRouter({
---
+++ src/views/Library/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>dd</div>
+</template>
+<script lang='ts'>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "home",
+  props: {
+    msg: String,
+  },
+});
+</script>
+<style scoped lang='scss'>
+@import "./index.scss"
+</style>
