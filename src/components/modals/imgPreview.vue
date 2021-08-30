c56a6e28bfe426e7d02ba1bbb7381f0c93736639
mouren <1832819123@qq.com>
2021-08-30 15:22:43 +0800

    修改图书中文文案

--- src/components/modals/imgPreview.vue
+++ src/components/modals/imgPreview.vue
@@ -66,8 +66,10 @@ export default defineComponent({
     .modal-container {
       width: 76% !important;
       height: auto !important;
-      padding: 30px !important;
+      padding: 20px !important;
       .close-btn {
+        right: 0 !important;
+        top: 0 !important;
         width: 24px !important;
       }
       .modal-content {
@@ -79,7 +81,7 @@ export default defineComponent({
           height: 300px !important;
         }
          .modal-right {
-           padding: 20px 0 0 0 !important;
+           padding: 20px !important;
            &-title {
             font-size: 16px !important;
             margin-bottom:10px;
@@ -91,6 +93,7 @@ export default defineComponent({
           &-digtials {
             font-size: 12px !important;
             position: relative !important;
+            bottom: 0 !important;
           }
          }
       }
