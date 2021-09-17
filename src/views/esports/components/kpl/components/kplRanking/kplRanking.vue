8bc993aeeea2bdf38e8211c8226b63ee08ea2afa
wanglan <809382327@qq.com>
2021-09-17 16:34:36 +0800

    弹框居中

--- src/views/esports/components/kpl/components/kplRanking/kplRanking.vue
+++ src/views/esports/components/kpl/components/kplRanking/kplRanking.vue
@@ -1,77 +1,79 @@
 <template>
   <div class="ranking-dialog">
-    <div class="dialog-content">
+    <div class="dialog-content flex">
       <img class="close" src="/match/close.png" alt="" @click="closeNet">
-      <div class="dialog-header margin-auto" v-if="data.titletips">
-        <img src="/kpl/bgi01.png" alt="">
-        <p>{{$t(data.titletips)}}</p>
-        <img src="/kpl/bgi01.png" alt="">
-      </div>
-      <div v-if="data.rankingtypeshow == 1">
-        <div class="rank-list flex">
-          <div class="rank-list-rank">
-            <div v-for="(item,index) in data.list" :key="item">
-              <div class="ranking-bumber flex" v-if="index < 5">
-                  <img :src="'/kpl/No'+item.pm+'.png'" alt="">
-                  <img :src="item.imgUrl ? item.imgUrl :'/kpl/header.png'" alt="">
-                  <div>
-                    <p>{{item.tickets}}{{$t('esports_kpl32')}}</p>
-                  </div>
-              </div>
-            </div>
-          </div>
-          <div class="rank-list-rank">
-            <div v-for="(item,index) in data.list" :key="item">
-              <div class="ranking-bumber flex" v-if="index > 4">
-                  <img :src="'/kpl/No'+item.pm+'.png'" alt="">
-                  <img :src="item.imgUrl ? item.imgUrl :'/kpl/header.png'" alt="">
-                  <div>
-                    <p>{{item.tickets}}{{$t('esports_kpl32')}}</p>
-                  </div>
-              </div>
-            </div>
-          </div>
+      <div>
+        <div class="dialog-header margin-auto" v-if="data.titletips">
+          <img src="/kpl/bgi01.png" alt="">
+          <p>{{$t(data.titletips)}}</p>
+          <img src="/kpl/bgi01.png" alt="">
         </div>
-        <div class="rank-media">
-          <div v-for="(item,index) in data.list" :key="item">
-              <div class="ranking-bumber flex">
-                  <img :src="'/kpl/No'+item.pm+'.png'" alt="">
-                  <img :src="item.imgUrl ? item.imgUrl :'/kpl/header.png'" alt="">
-                  <div>
-                    <p>{{item.tickets}}{{$t('esports_kpl32')}}</p>
-                  </div>
+        <div v-if="data.rankingtypeshow == 1">
+          <div class="rank-list flex">
+            <div class="rank-list-rank">
+              <div v-for="(item,index) in data.list" :key="item">
+                <div class="ranking-bumber flex" v-if="index < 5">
+                    <img :src="'/kpl/No'+item.pm+'.png'" alt="">
+                    <img :src="item.imgUrl ? item.imgUrl :'/kpl/header.png'" alt="">
+                    <div>
+                      <p>{{item.tickets}}{{$t('esports_kpl32')}}</p>
+                    </div>
+                </div>
               </div>
             </div>
+            <div class="rank-list-rank">
+              <div v-for="(item,index) in data.list" :key="item">
+                <div class="ranking-bumber flex" v-if="index > 4">
+                    <img :src="'/kpl/No'+item.pm+'.png'" alt="">
+                    <img :src="item.imgUrl ? item.imgUrl :'/kpl/header.png'" alt="">
+                    <div>
+                      <p>{{item.tickets}}{{$t('esports_kpl32')}}</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="rank-media">
+            <div v-for="(item,index) in data.list" :key="item">
+                <div class="ranking-bumber flex">
+                    <img :src="'/kpl/No'+item.pm+'.png'" alt="">
+                    <img :src="item.imgUrl ? item.imgUrl :'/kpl/header.png'" alt="">
+                    <div>
+                      <p>{{item.tickets}}{{$t('esports_kpl32')}}</p>
+                    </div>
+                </div>
+              </div>
+          </div>
+          <p class="timeword">{{$t('esports_kpl52')}}</p>
         </div>
-        <p class="timeword">{{$t('esports_kpl52')}}</p>
-      </div>
-      <div v-if="data.rankingtypeshow == 2" class="tipsrankingbox">
-          <p class="smolltips">{{$t('esports_kpl63')}}</p>
-          <p class="contentwords" v-html="$t('esports_kpl54')"></p>
-          <div class="btnbox flex">
-            <button @click="confirmbtn" v-if="data.rankingtypeshow == 3 || data.rankingtypeshow == 5 || data.rankingtypeshow == 6 || data.rankingtypeshow == 8 || data.rankingtypeshow == 10 || data.rankingtypeshow == 11 || data.rankingtypeshow == 12" class="btn1">{{$t('esports_kpl56')}}</button>
-            <button @click="confirmbtn" v-if="data.rankingtypeshow == 2" class="btn2">{{$t('esports_kpl55')}}</button>
-            <button @click="confirmbtn" v-if="data.rankingtypeshow == 2" class="btn2">{{$t('esports_kpl57')}}</button>
-            <button @click="confirmbtn" v-if="data.rankingtypeshow == 3" class="btn2">{{$t('esports_kpl58')}}</button>
-            <button @click="confirmbtn" v-if="data.rankingtypeshow == 6" class="btn2">{{$t('esports_kpl59')}}</button>
-            <button @click="confirmbtn" v-if="data.rankingtypeshow == 6" class="btn2">{{$t('esports_kpl60')}}</button>
-            <button @click="confirmbtn" v-if="data.rankingtypeshow == 6" class="btn2">{{$t('esports_kpl61')}}</button>
-            <button @click="confirmbtn" v-if="data.rankingtypeshow == 8" class="btn2">{{$t('esports_kpl62')}}</button>
-          </div>
-      </div>
-      <div v-if="data.rankingtypeshow == 3" class="tipsrankingbox">
-          <!-- <p class="smolltips">{{$t(data.titletips)}}</p> -->
-          <p class="contentwords" v-html="$t(data.content)"></p>
-          <div :class="data.rankingtypeshow == 3 ?'btnbox flex centerbtn':'btnbox flex'">
-            <button @click="confirmbtn" v-if="data.btn1show == 1" class="btn1">{{$t('esports_kpl56')}}</button>
-            <button @click="closeNet" v-if="data.btn2show == 1" class="btn2">{{$t('esports_kpl55')}}</button> <!-- 知道了 -->
-            <button @click="confirmbtn" v-if="data.btn2show == 2" class="btn2">{{$t('esports_kpl57')}}</button> <!-- 現在領取 -->
-            <button @click="confirmbtn" v-if="data.btn2show == 3" class="btn2">{{$t('esports_kpl58')}}</button> <!-- 現在兌換 -->
-            <button @click="confirmbtn" v-if="data.btn2show == 4" class="btn2">{{$t('esports_kpl59')}}</button> <!-- ATTA面具 -->
-            <button @click="confirmbtn" v-if="data.btn2show == 5" class="btn2">{{$t('esports_kpl60')}}</button> <!-- 現在兌換 -->
-            <button @click="confirmbtn" v-if="data.btn2show == 6" class="btn2">{{$t('esports_kpl61')}}</button> <!-- Loot 入場券 -->
-            <button @click="confirmbtn" v-if="data.btn2show == 7" class="btn2">{{$t('esports_kpl62')}}</button> <!-- 前往投票 -->
-          </div>
+        <div v-if="data.rankingtypeshow == 2" class="tipsrankingbox">
+            <p class="smolltips">{{$t('esports_kpl63')}}</p>
+            <p class="contentwords" v-html="$t('esports_kpl54')"></p>
+            <div class="btnbox flex">
+              <button @click="confirmbtn" v-if="data.rankingtypeshow == 3 || data.rankingtypeshow == 5 || data.rankingtypeshow == 6 || data.rankingtypeshow == 8 || data.rankingtypeshow == 10 || data.rankingtypeshow == 11 || data.rankingtypeshow == 12" class="btn1">{{$t('esports_kpl56')}}</button>
+              <button @click="confirmbtn" v-if="data.rankingtypeshow == 2" class="btn2">{{$t('esports_kpl55')}}</button>
+              <button @click="confirmbtn" v-if="data.rankingtypeshow == 2" class="btn2">{{$t('esports_kpl57')}}</button>
+              <button @click="confirmbtn" v-if="data.rankingtypeshow == 3" class="btn2">{{$t('esports_kpl58')}}</button>
+              <button @click="confirmbtn" v-if="data.rankingtypeshow == 6" class="btn2">{{$t('esports_kpl59')}}</button>
+              <button @click="confirmbtn" v-if="data.rankingtypeshow == 6" class="btn2">{{$t('esports_kpl60')}}</button>
+              <button @click="confirmbtn" v-if="data.rankingtypeshow == 6" class="btn2">{{$t('esports_kpl61')}}</button>
+              <button @click="confirmbtn" v-if="data.rankingtypeshow == 8" class="btn2">{{$t('esports_kpl62')}}</button>
+            </div>
+        </div>
+        <div v-if="data.rankingtypeshow == 3" class="tipsrankingbox">
+            <!-- <p class="smolltips">{{$t(data.titletips)}}</p> -->
+            <p class="contentwords" v-html="$t(data.content)"></p>
+            <div :class="data.rankingtypeshow == 3 ?'btnbox flex centerbtn':'btnbox flex'">
+              <button @click="confirmbtn" v-if="data.btn1show == 1" class="btn1">{{$t('esports_kpl56')}}</button>
+              <button @click="closeNet" v-if="data.btn2show == 1" class="btn2">{{$t('esports_kpl55')}}</button> <!-- 知道了 -->
+              <button @click="confirmbtn" v-if="data.btn2show == 2" class="btn2">{{$t('esports_kpl57')}}</button> <!-- 現在領取 -->
+              <button @click="confirmbtn" v-if="data.btn2show == 3" class="btn2">{{$t('esports_kpl58')}}</button> <!-- 現在兌換 -->
+              <button @click="confirmbtn" v-if="data.btn2show == 4" class="btn2">{{$t('esports_kpl59')}}</button> <!-- ATTA面具 -->
+              <button @click="confirmbtn" v-if="data.btn2show == 5" class="btn2">{{$t('esports_kpl60')}}</button> <!-- 現在兌換 -->
+              <button @click="confirmbtn" v-if="data.btn2show == 6" class="btn2">{{$t('esports_kpl61')}}</button> <!-- Loot 入場券 -->
+              <button @click="confirmbtn" v-if="data.btn2show == 7" class="btn2">{{$t('esports_kpl62')}}</button> <!-- 前往投票 -->
+            </div>
+        </div>
       </div>
     </div>
   </div>
