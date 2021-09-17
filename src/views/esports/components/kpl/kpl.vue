d8edb1f00d572f6b08812a440cbb354459b2d0ab
wangjinyi <wangjinyi143>
2021-09-17 10:25:17 +0800

    修改判断

--- src/views/esports/components/kpl/kpl.vue
+++ src/views/esports/components/kpl/kpl.vue
@@ -96,11 +96,11 @@
     <!-- 预告 -->
     <div class="kpl-notice kpl-notice-header margin-auto">
       <div class="notice-header flex">
-        <img :class="showkplindex != 0 ? 'up-next hideimg' : 'up-next'" src="/kpl/up.png" alt="" @click="beforeclick(showkplindex)">
+        <img :class="showkplindex != 0 ? 'up-next' : 'up-next hideimg'" src="/kpl/up.png" alt="" @click="beforeclick(showkplindex)">
         <img class="bgi01" src="/kpl/bgi01.png" alt="">
         <p v-if="kplinfo[showkplindex]">{{$t(computekpltimeshowword(kplinfo[showkplindex]))}}</p>
         <img class="bgi01" src="/kpl/bgi01.png" alt="">
-        <img :class="showkplindex != kplinfo.length - 1 ? 'up-next hideimg' : 'up-next'" src="/kpl/next.png" alt="" @click="nextclick(showkplindex)">
+        <img :class="showkplindex != kplinfo.length - 1 ? 'up-next' : 'up-next hideimg'" src="/kpl/next.png" alt="" @click="nextclick(showkplindex)">
       </div>
     </div>
     <div class="kpl-notice kpl-notice-bgi  margin-auto">
