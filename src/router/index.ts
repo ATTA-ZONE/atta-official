3e0ec97b9fbb8e8e23a2cd3f686514c461d61354
wangjinyi <wangjinyi143>
2021-09-17 14:25:49 +0800

    去掉弹框路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,14 +11,8 @@ const lpl = () => import("../views/esports/components/lpl/lpl.vue");
 const kpl = () => import("../views/esports/components/kpl/kpl.vue");
 const Xudongdong = () => import("../views/Xudongdong/index.vue");
 const Headportrait = () => import("../views/headportrait/index.vue");
-const kplModel = () => import("../views/esports/components/kpl/components/kplRanking/kplRanking.vue");
  
 const routes = [
-  {
-    path: "/kplModel",
-    name: "kplModel",
-    component: kplModel,
-  },
   {
     path: "/",
     name: "Home",
